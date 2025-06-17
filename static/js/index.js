2feff158eb8fc0695e2b8e76f489efb81081c017
purewave0 <purewave_@outlook.com>
2025-06-17 17:43:21 -0300

    feat(keyboard): add two-voices mode option

--- static/js/index.js
+++ static/js/index.js
@@ -51,30 +51,64 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    let activeNote = null;
+    let activeKeys = [];
     let currentOctave = 4;
 
-    const keys = document.getElementsByClassName('key');
+    // TODO: tuning system (equal temp., just int., pythagorean(maybe?))
+    // TODO: tuning standard (a440, a442...)
+    // TODO: (as a prerequisite for the above) calculate pitches according to the
+    // system and the standard
+    // TODO: keyboard controls (QWERTY... and, maybe, CDEF... + modifiers)
+    // TODO: chord mode?
+    const modeOption = document.getElementById('mode');
+    let isTwoVoices = modeOption.checked;
+    modeOption.addEventListener('change', () => {
+        isTwoVoices = modeOption.checked;
+        if (!isTwoVoices && activeKeys.length > 1) {
+            // reverting to one-voice mode. turn off the 2nd key
+            const secondActiveKey = activeKeys[1];
+            secondActiveKey.oscillator.stop();
+            secondActiveKey.key.classList.remove('active');
+            activeKeys.pop()
+        }
+    });
 
+    const keys = document.getElementsByClassName('key');
     for (const key of keys) {
         key.addEventListener('click', () => {
             const keyOctaveMultiplier = (key.dataset.octaveMultiplier)
                 ? Number(key.dataset.octaveMultiplier)
                 : 1;
 
-            if (activeNote) {
-                // TODO: add polyphonic mode
-                activeNote.oscillator.stop();
-                activeNote.key.classList.remove('active');
+            if (activeKeys.length > 0) {
+                const correspondentKeyIndex = activeKeys.findIndex(
+                    (activeKey) => activeKey.id === key.id
+                );
+                const clickedOnActiveKey = correspondentKeyIndex !== -1;
+                if (clickedOnActiveKey) {
+                    // clicking on any active key, no matter the mode, always turns it
+                    // off
+                    const correspondentKey = activeKeys[correspondentKeyIndex];
+                    correspondentKey.oscillator.stop();
+                    correspondentKey.key.classList.remove('active');
 
-                const pressedActiveNote = (
-                    activeNote.name === key.dataset.note
-                    && activeNote.keyOctaveMultiplier === keyOctaveMultiplier
-                )
-                activeNote = null;
-
-                if (pressedActiveNote) {
+                    activeKeys.splice(correspondentKeyIndex, 1);
                     return;
+                }
+
+                // is clicking on a new key. limit the number of active notes
+                // according to the current mode
+                if (
+                    !isTwoVoices
+                    || (isTwoVoices && activeKeys.length === 2)
+                ) {
+                    const lastIndex = activeKeys.length - 1;
+                    // in two-voices mode, this keeps the first one ("root") and stops
+                    // the last one
+                    const oldKey = activeKeys[lastIndex];
+                    oldKey.oscillator.stop();
+                    oldKey.key.classList.remove('active');
+                    activeKeys.pop();
                 }
             }
 
@@ -86,12 +120,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
             key.classList.add('active');
 
-            activeNote = {
-                name: key.dataset.note,
+            const activeKey = {
+                id: key.id,
+                note: key.dataset.note,
                 oscillator: playNote(frequency),
-                keyOctaveMultiplier: keyOctaveMultiplier,
+                octaveMultiplier: keyOctaveMultiplier,
                 key: key,
             }
+            activeKeys.push(activeKey);
         });
     }
 
@@ -116,18 +152,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    function replayActiveNote() {
-        const key = activeNote.key;
-        key.click();  // off
-        key.click();  // on
+    function replayActiveKeys() {
+        const activeKeyIds = activeKeys.map(key => key.id);
+
+        // can't iterate activeKeys for this because its size will change. use the
+        // stored key ids to directly replay them
+        for (const id of activeKeyIds) {
+            const key = document.getElementById(id);
+            key.click();  // off
+            key.click();  // on
+        }
     }
 
     const octaveOption = document.getElementById('octave');
     octaveOption.addEventListener('change', () => {
         currentOctave = Number(octaveOption.value);
-        if (activeNote) {
-            // replay the same note but with the new octave
-            replayActiveNote();
+        if (activeKeys.length > 0) {
+            // replay the same note(s) but with the new octave
+            replayActiveKeys();
         }
     });
 });
